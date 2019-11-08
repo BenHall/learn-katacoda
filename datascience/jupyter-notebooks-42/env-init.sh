99bc94e07b795c4cdb4d4913cb40f7fc17c40ce3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-11-08 14:24:44 +1100

    Load images/templates into project rather than globally.

--- datascience/jupyter-notebooks-42/env-init.sh
+++ datascience/jupyter-notebooks-42/env-init.sh
@@ -1,4 +1,7 @@
-curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-builder.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n openshift
-curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-deployer.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n openshift
-curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-quickstart.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n openshift
-curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-workspace.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n openshift
+oc login -u developer -p developer
+oc new-project myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/master/image-streams/s2i-minimal-notebook.json | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-builder.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-deployer.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-quickstart.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
+curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/develop/templates/notebook-workspace.json | sed -e 's/"Redirect"/"Allow"/' | oc apply -f - -n myproject
